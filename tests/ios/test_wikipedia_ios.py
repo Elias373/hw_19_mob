910068a0754d1c5eefb1e4727d5d5cec37969779
Illia Karcheuski <your.email@example.com>
2025-11-22 21:55:38 +0300

    android fix

--- tests/ios/test_wikipedia_ios.py
+++ tests/ios/test_wikipedia_ios.py
@@ -1,10 +1,21 @@
 import allure
 from selene import browser, have
+import time
 
 
-@allure.title("Simple iOS test")
-def test_ios_app_launch():
+@allure.title("Simple Android test")
+def test_search():
     browser.open('https://wikipedia.org')
 
-    with allure.step('Check page loaded'):
-        browser.element('.central-textlogo').should(have.text('Wikipedia'))
+    with allure.step('Type search'):
+        browser.element('#searchInput').type('Appium')
+
+
+        time.sleep(2)
+
+
+        search_button = browser.driver.find_element_by_css_selector('.pure-button')
+        browser.driver.execute_script("arguments[0].click();", search_button)
+
+    with allure.step('Verify content found'):
+        browser.element('#firstHeading').should(have.text('Appium'))
