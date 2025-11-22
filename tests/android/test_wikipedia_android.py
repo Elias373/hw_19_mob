bfee97addd45611d0d685772df4e7e012a5b21cb
Illia Karcheuski <your.email@example.com>
2025-11-22 22:54:05 +0300

    andr fix

--- tests/android/test_wikipedia_android.py
+++ tests/android/test_wikipedia_android.py
@@ -3,11 +3,16 @@ from selene import browser, be
 from appium.webdriver.common.appiumby import AppiumBy
 
 
-@allure.title("Android app test - simple interaction")
-def test_android_app_interaction(mobile_management):
-    with allure.step('Wait for app to load'):
-        browser.element((AppiumBy.XPATH, "//*")).should(be.visible)
+@allure.title("Android Wikipedia search test")
+def test_android_wikipedia_search(mobile_management):
+    with allure.step("Click search button"):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
 
-    with allure.step('Take screenshot of loaded app'):
-        # Просто проверяем что приложение запустилось
-        pass
+    with allure.step("Type search query"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Python")
+
+    with allure.step("Click on search result"):
+        browser.element((AppiumBy.XPATH, "//*[@text='Python (programming language)']")).click()
+
+    with allure.step("Verify article opened"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/page_contents_container")).should(be.visible)
